5183a56bd553c40480a75af5224ac9ad974b1111
Alison Cameron <camerona2@carleton.edu>
2021-05-23 13:48:02 -0500

    Need to add traceback

--- main.py
+++ main.py
@@ -4,22 +4,23 @@
 
 import sys
 import math
-import numpy
-import pandas
+import numpy as np
 
 #Read in the input
 def read_input(filename):
   nucleotides = []
   with open(filename, "r") as input_file:
     rna_strand = input_file.readline()
-  
+
   for ch in rna_strand:
     #Check if input is valid
     if ch in ["A", "C", "G", "U"]:
       nucleotides.append(ch)
+    elif ch in ["\n", " "]:
+        continue
     else:
       sys.exit("Input is not valid")
-      
+
   return nucleotides
 
 #Initializing the Matrix
@@ -28,9 +29,9 @@ def create_matrix(n):
   for i in range(n):
     scores_i = []
     for j in range(n):
-      scores_i.append(None)
+      scores_i.append(0)
     opt_score.append(scores_i)
-  
+
   for i in range(n):
     opt_score[i][i] = 0
     if i >= 1:
@@ -39,17 +40,41 @@ def create_matrix(n):
   return opt_score
 
 #Filling in the Matrix
+def populate_matrix(mat, rna):
+    for j in range(1, len(mat)):
+        for i in reversed(range(0, j)):
+            fold_nums = []
+            for k in range(i, j):
+                num = mat[i][k-1] + mat[k+1][j-1] + score(rna[k], rna[j])
+                fold_nums.append(num)
+            fold_nums.append(mat[i][j-1])
+            mat[i][j] = max(fold_nums)
+    return mat
+
 
 #Display Matrix neatly
 def print_matrix(mat):
   opt_score_array = np.array(mat)
   print(opt_score_array)
 
+#determine if any two bases are complementary
+def score(base1, base2):
+    if base1 == "A" and base2 == "U":
+        return 1
+    elif base1 == "U" and base2 == "A":
+        return 1
+    elif base1 == "C" and base2 == "G":
+        return 1
+    elif base1 == "G" and base2 == "C":
+        return 1
+    else:
+        return 0
+
 
 def main():
   rna = read_input("input.txt")
   opt_score = create_matrix(len(rna))
+  opt_score = populate_matrix(opt_score, rna)
   print_matrix(opt_score)
-  #print pandas.DataFrame(opt_score)
-  
+
 main()
